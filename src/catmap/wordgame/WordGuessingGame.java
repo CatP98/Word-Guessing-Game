f9fa3782c4623fb350ccbf1b60bf5a8d071fdb92
catarina p <catarinampestana@ua.pt>
2024-11-29 12:05:13 +0000

    Loop is complete. To do: Improve the user interaction displays, create themed lists of words (countries, animals, food, jobs, etc)  and allow the user to pick a theme, before starting the loop

--- src/catmap/wordgame/WordGuessingGame.java
+++ src/catmap/wordgame/WordGuessingGame.java
@@ -8,50 +8,63 @@ public class WordGuessingGame {
     private char[] guessedLetters;
     private int attempts;
 
-    public WordGuessingGame(String[] wordsList, int maxAttempts){
+    public WordGuessingGame(String[] wordsList, int maxAttempts) {
         Random rand = new Random();
         this.wordToGuess = wordsList[rand.nextInt(wordsList.length)];
         this.guessedLetters = new char[this.wordToGuess.length()];
-        for(int i = 0; i < this.guessedLetters.length; i++){
+        for (int i = 0; i < this.guessedLetters.length; i++) {
             this.guessedLetters[i] = '_';
-
-
         }
         this.attempts = maxAttempts;
     }
 
-    public void play(){
-        System.out.println("The word is: " + wordToGuess);
-        Scanner scanner = new Scanner(System.in);
+    public void play() {
+        //System.out.println("The word is: " + wordToGuess);
 
-        while (attempts > 0) {
-            System.out.println("You have "+ attempts + " left.");
+        Scanner scanner = new Scanner(System.in);
+        boolean gameIsOver = false;
+
+        while (attempts > 0 && !gameIsOver) {
+            displayState();
+            System.out.println("\nYou have " + attempts + " left.");
             System.out.println("Guess a letter: ");
+
             char guess = scanner.nextLine().toLowerCase().charAt(0);
 
-            if(!processGuess(guess)){
-                System.out.println("That guess was imcorrect.");
+            if (processGuess(guess)) {
+                System.out.println("*** Correct!. Letters guessed: " + guessedLetters + "***");
+                if (new String(guessedLetters).equals(wordToGuess)) {
+                    System.out.println("You have guessed the word!");
+                    //break;
+                    gameIsOver = true;
+                }
+
+            } else {
+                System.out.println("--- That guess was incorrect. ---");
                 attempts--;
             }
-            else {
-                //guessedLetters.add(guess);
-                System.out.println("Correct!. Letters guessed: " + guessedLetters);
-            }
-
+        }
+        if(attempts == 0){
+            System.out.println("Maximum attempts reached... The word was: \"" + wordToGuess + "\" Game Over!");
 
         }
-
     }
 
-    private boolean processGuess(char letter){
+    private boolean processGuess ( char letter){
         boolean letterFound = false;
 
-        for(int i = 0; i < wordToGuess.length(); i++){
-            if(wordToGuess.charAt(i) == letter) {
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            if (wordToGuess.charAt(i) == letter) {
                 guessedLetters[i] = letter;
                 letterFound = true;
             }
         }
         return letterFound;
     }
+
+    private void displayState(){
+        System.out.println("Guessed Letters: " + new String(guessedLetters));
+    }
 }
+
+
